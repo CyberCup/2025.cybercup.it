a3adb2a41bc050cd24a469728ec28d15123f63c2
VaiTon <eyadlorenzo@gmail.com>
2025-01-18 15:29:19 +0100

    Fix redirect

--- src/routes/calendar/+page.ts
+++ src/routes/calendar/+page.ts
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+import { base } from '$app/paths';
 
 export const load: PageLoad = async () => {
-	redirect(307, '/calendar/2025');
+	redirect(307, base + '/calendar/2025');
 };
--- src/routes/scoreboard/+page.ts
+++ src/routes/scoreboard/+page.ts
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+import { base } from '$app/paths';
 
 export const load: PageLoad = async () => {
-	redirect(307, '/scoreboard/2025');
+	redirect(307, base + '/scoreboard/2025');
 };
