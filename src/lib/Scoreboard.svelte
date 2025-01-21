f2fe439371d3188daa7d8dbcf3c8491aac59a1fa
Marco Ceccon <me@marco.win>
2025-01-21 17:48:34 +0100

    Updated scoreboard and calendar design

--- src/lib/Scoreboard.svelte
+++ src/lib/Scoreboard.svelte
@@ -10,14 +10,14 @@
 	let orderedScores = $derived(sortScoreboard(scoreboard));
 </script>
 
-<table class="table table-zebra table-pin-rows table-lg">
+<table class="table table-zebra table-pin-rows table-lg border-separate border-spacing-y-2">
 	<thead>
-		<tr>
+		<tr class="bg-primary text-white">
 			<th></th>
 			<th></th>
 			<th>Team</th>
 			<th>Affiliazione</th>
-			<th>Punteggio</th>
+			<th class="text-center">Punteggio</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -25,18 +25,34 @@
 			{@const gold = i === 0}
 			{@const silver = i === 1}
 			{@const bronze = i === 2}
-			<tr class:gold class:silver class:bronze>
-				<td>{i + 1}</td>
-				<td class="w-24 p-2"><img src="{base}/logos/{team.logo}" alt={team.team_name} /></td>
+			<tr class:gold class:silver class:bronze class="rounded-lg">
+				{#if i < 3}
+					<td class="px-8 text-2xl font-bold">{i + 1}</td>
+				{:else}
+					<td class="px-8 text-lg font-semibold">{i + 1}</td>
+				{/if}
 				<td>
+					<img
+						src="{base}/logos/{team.logo}"
+						class="grid aspect-square h-20 items-center justify-center rounded-lg bg-white object-contain p-2"
+						alt={team.team_name}
+					/>
+				</td>
+				<td class="text-lg">
 					{#if team.website != null}
-						<a href={team.website} class="link">{team.team_name}</a>
+						<a href={team.website} class="link flex items-center">
+							{team.team_name}
+
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 ml-1">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+							</svg>
+						</a>
 					{:else}
 						{team.team_name}
 					{/if}
 				</td>
 				<td>{team.affiliation}</td>
-				<td>{team.score}</td>
+				<td class="text-center font-semibold">{team.score}</td>
 			</tr>
 		{/each}
 	</tbody>
