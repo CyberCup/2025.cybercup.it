ad3dc8d96e13bcdd8a6bfd134d652dbfcc84b1c3
Marco Ceccon <me@marco.win>
2025-02-11 16:42:29 +0100

    Mobile calendar fix

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -12,9 +12,9 @@
 	<meta name="description" content="Calendario delle gare di CyberCup.IT" />
 </svelte:head>
 
-<h2 class="mb-12 text-center text-3xl font-bold">Calendario CTF CyberCup.IT 2025:</h2>
+<h2 class="mb-6 text-center text-3xl font-bold">Calendario CTF CyberCup.IT 2025:</h2>
 
-<Alert class="mx-auto w-max" type="warning">
+<Alert class="mx-auto max-w-max mb-6" type="warning">
 	<p>Il calendario è in fase di definizione. Le date potrebbero subire variazioni.</p>
 </Alert>
 
