9ea45b48f4d79ca9fb24d0f6d2dc791ab4546429
VaiTon <eyadlorenzo@gmail.com>
2026-03-15 14:41:46 +0100

    chore: Upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 import Icons from 'unplugin-icons/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
 		Icons({
 			compiler: 'svelte',
